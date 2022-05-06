030d6e5dada647b5ac776614e60f54bf5ed0201b
Kirill Nevedrov <kirill.nevedrov@gmail.com>
2022-05-06 20:01:06 +0300

    python / lesson 10

--- python/lesson_10/app.py
+++ python/lesson_10/app.py
@@ -41,12 +41,10 @@ def get_post_feed(id: int, limit: int = 10, db: Session = Depends(get_db)):
 
 @app.get("/post/recommendations/", response_model=List[PostGet])
 def get_post_recommendations(id: int, limit: int = 10, db: Session = Depends(get_db)):
-    post_ids = db \
-                .query(Feed.post_id) \
-                .filter(Feed.action == 'like') \
-                .group_by(Feed.post_id) \
-                .order_by(desc(func.count(Feed.post_id))) \
-                .limit(limit) \
-                .all()
-
-    return db.query(Post).filter(Post.id.in_(post_ids)).all()
+    return db.query(Post.id, Post.text, Post.topic) \
+        .join(Feed, Feed.post_id == Post.id) \
+        .filter(Feed.action == 'like') \
+        .group_by(Post.id) \
+        .order_by(func.count(Post.id).desc()) \
+        .limit(limit) \
+        .all()
